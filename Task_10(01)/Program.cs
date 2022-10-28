9994d00e62b7872217a0703af6a0f7688a974f5f
Alexander <alex17.05.84@yandex.ru>
2022-10-28 13:52:15 +0300

    Task 10 done

---
+++ Task_10(01)/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+void SecondNum(int number)
+{
+    int[] arraytdn = new int[3];
+    for (int i = 2; i >= 0; i--)
+    {
+        arraytdn[i] = number % 10;
+        number /= 10;
+    }
+    Console.WriteLine("Второе число = " + arraytdn[1]);
+}
+
+Console.Write("Введите трехзначное число: ");
+SecondNum(int.Parse(Console.ReadLine() ?? "0"));
+
+
+
+
+
+
+
+
+
+
